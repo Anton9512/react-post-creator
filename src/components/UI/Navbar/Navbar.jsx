4c3868645fdf7d3044eb829a01e9f8ac28a970b2
Anton9512 <anton.mail9512@gmail.com>
2022-03-03 13:00:44 +0300

    add styles

--- src/components/UI/Navbar/Navbar.jsx
+++ src/components/UI/Navbar/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+import classes from './Navbar.module.css'
 
 const Navbar = () => {
     return (
         <div className='navbar'>
             <div className="navbar__links">
-                <Link to="/about">О приложении</Link>
-                <Link to="/posts">Посты</Link>
+                <Link className={classes.navbar__btns} to="/about">О приложении</Link>
+                <Link className={classes.navbar__btns} to="/posts">Посты</Link>
             </div>
         </div>
     );
